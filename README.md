83f36b78af621778907d6546c6a3bf8f24a44c4d
Naveq <naveq.yt@gmail.com>
2020-12-27 21:45:34 +0100

    Our website :D
    Signed-off-by: Naveq <naveq.yt@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Alt-F4-Team
+This is our website, available [here](https://alt-f4-team.xyz)
---
+++ js/index.js
@@ -0,0 +1,34 @@
+const logo_section = document.getElementById('logo');
+const backtotop = document.getElementById('backtotop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > logo_section.offsetHeight) {
+        backtotop.style.display = 'block';
+        backtotop.style.opacity = 1;
+    } else {
+        backtotop.style.display = 'none'
+        backtotop.style.opacity = 0;
+    }
+})
+
+let projectsList = []
+
+for (let i = 0; i < projectsList.length; i++) {
+    let projects = document.createElement("div")
+    projects.className = "project";
+    projects.innerHTML = `
+          <a target="_blank" href="${projectsList[i]["url"]}">
+            <img src="img/${projectsList[i]["screenshot"]}" height="256"></img>
+          <p>${projectsList[i]["name"]}</p></a>
+          `
+    document.getElementById("projects").appendChild(projects)
+}
+if (projectsList.length === 0) {
+    let projects = document.createElement("div");
+    projects.innerHTML = '<b>OOF</b><br /><br />There aren\'t any projects yet...';
+    document.getElementById("projects").appendChild(projects);
+}
+
+backtotop.addEventListener('click', () => {
+    window.scrollTo(0, 0)
+})
